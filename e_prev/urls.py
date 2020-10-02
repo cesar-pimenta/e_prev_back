c3f85032b0e4b4e09cb6cfc0d7a8183127cebf65
cesar-pimenta <cesar.pimenta@live.com>
2020-10-02 20:44:37 -0300

    deploy

--- e_prev/urls.py
+++ e_prev/urls.py
@@ -7,6 +7,8 @@ from .views import UserViewSet, GroupViewSet
 from store.views import OrderViewSet, ItemViewSet
 from stock.views import ProductViewSet
 
+from rest_framework.authtoken import views
+
 
 router = routers.DefaultRouter()
 router.register(r'users', UserViewSet)
@@ -19,5 +21,6 @@ router.register(r'products', ProductViewSet)
 urlpatterns = [
     path('', include(router.urls)),
     path('admin/', admin.site.urls),
+    path('api-token-auth/', views.obtain_auth_token, name='api-token-auth'),
     path('api-auth/', include('rest_framework.urls', namespace='rest_framework'))
 ]
