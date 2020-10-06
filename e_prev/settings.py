77b488384822d2f3ad938be53bbe85296c1f6766
cesar-pimenta <cesar.pimenta@live.com>
2020-10-06 02:48:34 -0300

    dsadds

--- e_prev/settings.py
+++ e_prev/settings.py
@@ -116,6 +116,8 @@ REST_FRAMEWORK = {
     'PAGE_SIZE': 10
 }
 
+CORS_ORIGIN_ALLOW_ALL = True
+CORS_ALLOW_CREDENTIALS = True
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:3000",
     "http://127.0.0.1:3000",
